c66e2a61017c8f39fb36f5f4262c6445a4e86d05
archy gupta <archygupta22@gmail.com>
2022-09-05 19:34:26 +0530

    Update and rename 7. Sliding Window Maximum L_239 to 7. Sliding Window Maximum L_239.java

--- LeetCode/30 day leetcoding challenge - November/Week 4: November 22nd - November 28th/7. Sliding Window Maximum L_239.java
+++ LeetCode/30 day leetcoding challenge - November/Week 4: November 22nd - November 28th/7. Sliding Window Maximum L_239.java
@@ -48,21 +48,28 @@ Remove redundant elements and the queue should store only elements that need to 
 
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        Deque < Integer > q = new LinkedList < Integer > ();
-        int res[] = new int[nums.length - k + 1];
-        int idx = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (!q.isEmpty() && q.getFirst() < i - k + 1) {
-                q.removeFirst();
-            }
-            while (!q.isEmpty() && nums[q.getLast()] < nums[i]) {
-                q.removeLast();
-            }
-            q.add(i);
-            if (i >= k - 1) {
-                res[idx++] = nums[q.getFirst()];
-            }
+        Deque<Integer> q = new LinkedList<Integer>();
+        int[] res = new int[nums.length-k+1];
+        int idx=0;
+        for(int i=0;i<nums.length;i++){
+          //remove outside window indexes
+          if(!q.isEmpty() && q.getFirst()<i-k+1){
+            q.removeFirst();
+          }
+          
+          //remove less value indexes than current
+          while(!q.isEmpty() && nums[q.getLast()]<nums[i]){
+            q.removeLast();
+          }
+          
+          //add current to queue
+          q.add(i);
+          
+          //add to result, if current window size is k
+          if(i>=k-1){
+            res[idx++]=nums[q.getFirst()];
+          }
         }
-        return res;
+      return res;
     }
 }
